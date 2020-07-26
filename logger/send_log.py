a59444e09b55632694b1770906c2cd4137e08929
yemou <meeyou@tuta.io>
2020-07-26 03:03:33 +0000

    clean up comments

--- logger/send_log.py
+++ logger/send_log.py
@@ -1,7 +1,7 @@
-""" Very simple function to send logs to the correct channel """
+"""Very simple function to send logs to the correct channel"""
 
 
 async def send_to_log(bot, embed, channel_id):
-    """ Send embed to channel corresponding with channel id """
+    """Send embed to channel corresponding with channel id"""
     channel = bot.get_channel(channel_id)
     await channel.send(embed=embed)
